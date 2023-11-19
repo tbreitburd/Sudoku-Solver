c0bc9434450b562bef77b5a10c10e76a49288a27
Thomas Breitburd <thomasbreitburd@dhcp-10-248-23-141.eduroam.wireless.private.cam.ac.uk>
2023-11-19 13:14:07 +0000

    Added docs to preprocessing and solve_sudoku and set up the Doxyfile accordingly

---
+++ src/preprocessing.py
@@ -0,0 +1,6 @@
+"""!@file preprocessing.py
+@brief Module containing tools for preprocessing the sudoku txt file into a useable array
+
+@details This module contains tools to load the sudoku txt file information into a useable array.
+@author Created by T.Breitburd on 19/11/2023
+"""
--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -1,3 +1,10 @@
+"""!@file solve_sudoku.py
+@brief This file contains the main sudoku solving code. .
+
+@details Using functions from modules ..., it takes in a sudoku in txt format (with specific formatting), solves it using ..., and returns the solved sudoku in the same format as the input.
+@author Created by T.Breitburd on 19/11/2023
+"""
+
 import sys
 import configparser as cfg
 
@@ -5,3 +12,4 @@ input_file = sys.argv[1]
 
 config = cfg.ConfigParser()
 config.read(input_file)
+
