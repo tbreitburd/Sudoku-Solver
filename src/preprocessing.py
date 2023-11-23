e785ac56988cfe954f5ca40fcbd7b5f97b8b8ed4
Thomas Breitburd <thomasbreitburd@dhcp-10-248-207-96.eduroam.wireless.private.cam.ac.uk>
2023-11-23 00:02:31 +0000

    Added a tryout function to see if the pre commit works

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,12 @@
 """!@file preprocessing.py
-@brief Module containing tools for preprocessing the sudoku txt file into a useable array
+@brief Module containing tools for preprocessing
+the sudoku txt file into a useable array
 
-@details This module contains tools to load the sudoku txt file information into a useable array.
+@details This module contains tools to load
+the sudoku txt file information into a useable array.
 @author Created by T.Breitburd on 19/11/2023
 """
+
+
+def test(x):
+    return x + 1
---
+++ test/test_preproc.py
@@ -0,0 +1,6 @@
+# test preprocessing.py
+from src import preprocessing
+
+
+def test_preproc():
+    assert preprocessing.test(2) == 3
