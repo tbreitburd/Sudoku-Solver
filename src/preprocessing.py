c21b55cbd5e89b4b2e4957f6aea38f876d9825ca
Thomas Breitburd <thomasbreitburd@dhcp-10-248-221-229.eduroam.wireless.private.cam.ac.uk>
2023-11-26 12:09:29 +0000

    Wrote the start of the load_sudoku function (preprocessing)

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,3 +10,14 @@ the sudoku txt file information into a useable array.
 
 def test(x):
     return x + 1
+
+
+def load_sudoku(path):
+    """!@brief Load the sudoku txt file into a useable array
+
+    @param path Path to the sudoku txt file
+    @return A 9x9 numpy array containing the sudoku numbers
+    """
+    with open(path, "r") as f:
+        sudoku = [[int(x) for x in line.split()] for line in f]
+    return sudoku
