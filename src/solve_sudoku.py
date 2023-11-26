7fecdd8cdf1cc1d8dfc81bd68e8433f394fdebc9
Thomas Breitburd <thomasbreitburd@dhcp-10-248-221-229.eduroam.wireless.private.cam.ac.uk>
2023-11-26 12:12:03 +0000

    Added the load_sudoku function and added a print statement to see the output

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -1,15 +1,22 @@
 """!@file solve_sudoku.py
 @brief This file contains the main sudoku solving code. .
 
-@details Using functions from modules ..., it takes in a sudoku in txt format (with specific formatting), solves it using ..., and returns the solved sudoku in the same format as the input.
+@details Using functions from modules ...,
+it takes in a sudoku in txt format (with specific formatting),
+solves it using ...,
+and returns the solved sudoku in the same format as the input.
 @author Created by T.Breitburd on 19/11/2023
 """
 
 import sys
 import configparser as cfg
+import preprocessing as preproc
 
 input_file = sys.argv[1]
 
 config = cfg.ConfigParser()
 config.read(input_file)
 
+sudoku = preproc.load_sudoku(config["Input"]["sudoku1"])
+
+print(sudoku)
