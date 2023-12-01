c3425663b9edfa66554a0631901fec51a8ab7bc2
Thomas Breitburd <thomasbreitburd@dhcp-10-248-195-239.eduroam.wireless.private.cam.ac.uk>
2023-12-01 22:19:58 +0000

    Added a 2nd test function for the markup function to test error traps

--- test/test_markup.py
+++ test/test_markup.py
@@ -1,4 +1,6 @@
 from src import solver_tools, preprocessing
+import unittest
+import numpy as np
 
 
 def test_markup():
@@ -7,3 +9,11 @@ def test_markup():
     )
     assert markup_tester[3][4] == [1, 3, 4, 8]
     assert markup_tester[6][0] == [8]
+
+
+empty_sudoku = np.zeros((9, 9), dtype=int)
+
+
+def test_markup_2():
+    with unittest.TestCase().assertRaises(ValueError):
+        solver_tools.markup(empty_sudoku)
