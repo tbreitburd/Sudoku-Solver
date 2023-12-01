45d743907c8766d17504b5426a5c27d0081a15a9
Thomas Breitburd <thomasbreitburd@dhcp-10-248-195-239.eduroam.wireless.private.cam.ac.uk>
2023-12-01 22:16:31 +0000

    Added error traps to the markup function in solver_tools

--- src/solver_tools.py
+++ src/solver_tools.py
@@ -2,7 +2,8 @@
 @brief Module containing tools for solving the sudoku.
 
 @details This module contains tools to solve the sudoku.
-One of them is
+One of them is a function that creates a markup of possible values,
+for the sudoku cells.
 @author Created by T.Breitburd on 19/11/2023
 """
 import numpy as np
@@ -11,6 +12,19 @@ import pandas as pd
 
 
 def markup(sudoku):
+    """!@brief Create a markup for the sudoku.
+
+    @details This function takes in a sudoku.
+    And for each empty cell of that sudoku,
+    lists the possible values for that cell.
+    And it returns all these possible values in a dataframe,
+    with corresponding row and column indexes.
+    """
+    if all(x == 0 for x in np.ravel(sudoku[:][:])):
+        raise ValueError("The sudoku is empty")
+    if all(x != 0 for x in np.ravel(sudoku[:][:])):
+        raise ValueError("All cells are filled, the sudoku is already solved")
+
     markup = pd.DataFrame(index=range(9), columns=range(9))
     for row in range(9):
         for col in range(9):
@@ -23,4 +37,5 @@ def markup(sudoku):
                     and i not in (np.ravel(pp.box(sudoku, row, col)))
                 ]
                 markup[col][row] = cell_markup
+
     return markup
