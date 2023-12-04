ee443cffadb0a9315095ec6aba35c2eea00b452f
Thomas Breitburd <thomasbreitburd@Thomass-MacBook-Air-77.local>
2023-12-04 16:37:57 +0000

    Added a break condition to the solve_sudoku code so that it stops if the markup updating can solve the sudoku when it is easy enough

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -20,7 +20,7 @@ input_file = sys.argv[1]
 config = cfg.ConfigParser()
 config.read(input_file)
 
-sudoku = preproc.load_sudoku(config["Input"]["sudoku1"])
+sudoku = preproc.load_sudoku(config["Input"]["sudoku_easy"])
 
 markup_0 = st.markup(sudoku)
 markup_1 = pd.DataFrame(index=range(9), columns=range(9))
@@ -30,8 +30,7 @@ while not np.array_equal(markup_0.values, markup_1.values):
     for row in range(9):
         for col in range(9):
             if len(markup_0[col][row]) == 1:
-                print(markup_0[col][row][0])
                 sudoku[row][col] = markup_0[col][row][0]
+    if all(x != 0 for x in np.ravel(sudoku[:][:])):
+        break
     markup_1 = st.markup(sudoku)
-
-print(sudoku)
