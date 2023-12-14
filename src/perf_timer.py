cca2c15f663c8ab76c6406e0a9a56a09365b6bed
Thomas Breitburd <tmb76@cam.ac.uk>
2023-12-14 15:05:09 +0100

    Added a perf_timer file that goes through a csv of different sudokus, and times the different types of backtracking algorithm

---
+++ src/perf_timer.py
@@ -0,0 +1,114 @@
+from . import solver_tools as st
+import numpy as np
+import pandas as pd
+import time
+
+
+def solve_sudoku(sudoku, backtracking_type):
+    # Create a markup dataframe of possible values for the sudoku
+    # and initialise a second markup dataframe, to compare the updated markup
+    # with the first one.
+    markup_0 = st.markup(sudoku)
+    markup_1 = pd.DataFrame(index=range(9), columns=range(9))
+
+    # We want to put any unique possible value that the markup
+    # finds in the sudoku.
+    # And update the markup following that change,
+    # until the markup doesn't change.
+    while not np.array_equal(markup_0.values, markup_1.values):
+        markup_0 = st.markup(sudoku)
+        for row in range(9):
+            for col in range(9):
+                if len(markup_0[col][row]) == 1:
+                    sudoku[row][col] = markup_0[col][row][0]
+        if all(x != 0 for x in np.ravel(sudoku[:][:])):
+            break
+        markup_1 = st.markup(sudoku)
+
+    # Check if the sudoku is valid after the marking up
+    valid, message = st.check_sudoku(sudoku)
+    if not valid:
+        # fmt: off
+        raise RuntimeError("The sudoku is no longer valid after markup: "
+                           + message)
+        # fmt: on
+
+    # If the sudoku is already solved by this point, print it.
+    if all(x != 0 for x in np.ravel(sudoku[:][:])):
+        return None
+    else:
+        # If the sudoku is not solved yet, we need to use backtracking.
+        # Now get the indexes of the cells that still have more than
+        # one possible value, based on the markup file,
+        # and store them in a numpy array as pairs.
+        backtrack_cells = np.where(markup_1.map(len) > 1)
+
+        # fmt: off
+        if backtracking_type == "forward":
+            # Simply go from left to right, top to bottom
+            backtrack_cells = np.array([backtrack_cells[1],
+                                        backtrack_cells[0]]).T
+        elif backtracking_type == "backward":
+            # Go from right to left, bottom to top
+            backtrack_cells = np.array([backtrack_cells[1],
+                                        backtrack_cells[0]]).T[::-1]
+        elif backtracking_type == "ordered":
+            # Sort these cells by the number of possible values they have,
+            # in ascending order.
+            # This is to possibly make the backtracking algorithm faster.
+            # From https://learnpython.com/blog/python-custom-sort-function/
+            def sorting_key(cell):
+                return len(markup_1[cell[0]][cell[1]])
+            backtrack_cells = np.array([backtrack_cells[1],
+                                        backtrack_cells[0]]).T
+        # fmt: on
+            backtrack_cells = sorted(backtrack_cells, key=sorting_key)
+
+        # Now use the backtracking algorithm to solve the sudoku,
+        # only going through the possible values listed in the identified cells
+        # above.
+        # If the algorithm is successful, print the solved sudoku.
+        # Otherwise the backtracking algorithm will either raise an error or
+        # return False, in which case we raise an error.
+        if st.backtrack_alg(sudoku, markup_1, backtrack_cells, 0):
+            return None
+        else:
+            return None
+
+
+# Read sudokus from sudokus.csv
+sudokus_df = pd.read_csv("sudokus/sudokus.csv", header=None)
+time_forward = []
+time_backward = []
+time_ordered = []
+
+
+# Iterate over each sudoku
+for backtracking_type in ["forward", "backward", "ordered"]:
+    for sudoku_str in sudokus_df[0]:
+        sudoku = np.array([int(char) for char in sudoku_str]).reshape((9, 9))
+        # Convert the string representation of the sudoku to a 9x9 numpy array
+        # Solve the sudoku using the three types of backtracking
+
+        start_time = time.time()
+        solution = solve_sudoku(sudoku, backtracking_type)
+        end_time = time.time()
+
+        duration = end_time - start_time
+
+        if backtracking_type == "forward":
+            time_forward.append(duration)
+        elif backtracking_type == "backward":
+            time_backward.append(duration)
+        elif backtracking_type == "ordered":
+            time_ordered.append(duration)
+
+# fmt: off
+# Print the average time taken for each type of backtracking
+print("Average time taken for forward backtracking: " +
+      str(np.mean(time_forward)))
+print("Average time taken for backward backtracking: " +
+      str(np.mean(time_backward)))
+print("Average time taken for ordered backtracking: " +
+      str(np.mean(time_ordered)))
+# fmt: on
